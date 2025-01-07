28146bdac073de187538068ad852d1d6e6a36de2
Veli Furkan TURKOGLU <74744821+FurkanCodes@users.noreply.github.com>
2025-01-07 14:36:06 +0000

    query string handled

--- test.js
+++ test.js
@@ -6,14 +6,15 @@ router.get('/', (req, res) => {
   res.end('Hello from the root endpoint');
 });
 
-router.get('/user/:name', (req, res) => {
-  console.log(req.name)  //
-  res.end(`Hello, ${req.params.name}!`);
-});
 
 router.get('/user/:age/class/:subject', (req, res) => {
   console.log(req.params)  // Should now correctly show { age: '21', subject: 'Mathematics' }
   res.end(`You're ${req.params.age} years old, and you're studying ${req.params.subject}.`);
 });
+
+router.get("/user/:name", (req, res) => {
+ 
+  res.end(`You're ${req.params.name}.`);
+})
 
 run(router, 3000);
